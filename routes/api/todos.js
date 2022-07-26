a8c054245d1d63a9ef5077ace15ba16c197a42f9
David Woo <wcj617@gmail.com>
2022-07-25 18:59:42 -0700

    search user list by userID

--- routes/api/todos.js
+++ routes/api/todos.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const lists = require("../../List");
 
-router.get('/userId', (req, res) => {
-    const found = lists.some(list => list.userId === req.params.userId);
-
+router.get('', (req, res) => {
+    const found = lists.some(list => list.userId === req.query.userId);
     if (found) {
-        res.json(lists.filter(list => list.userId === req.params.userId));
+        res.json(lists.filter(list => list.userId === req.query.userId));
     } else {
-        res.status(400).json({ msg: `No match information with member of ${req.params.userId}` })
+        res.status(400).json({ msg: `No match information with member of ${req.query.userId}` })
+
     }
 });
 
